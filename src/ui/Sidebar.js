2ff471a97a04abc06b0d6f100f522f67d7754cbd
B0rjajas <xabierlaita@gmail.com>
2024-08-29 18:00:00 +0200

    cambios

--- src/ui/Sidebar.js
+++ src/ui/Sidebar.js
@@ -9,7 +9,7 @@ return(
         <p className=" text-white text-2xl tracking-wide text-center font-bold uppercase">Restaurente APP de MAC</p>
         <p className="mt-3 text-gray-600">Administra  tu restaurante</p>
         
-        <nav>
+        <nav className="mt-10 ">
             <Link to="/">Ordenes</Link>
             <NavLink className="p-1 text-gray-400 block hover:bg-yellow-500 hover:text-gray-500" activeClassname="text-yellow-500" exact to="/menu">Menú</NavLink>
         </nav>
