ddda3e527c4222da70e54dce153c8304bfcffdbe
B0rjajas <xabierlaita@gmail.com>
2024-08-30 17:03:36 +0200

    cambios

---
+++ firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBDnCfhXfZL8eq1sDd9K4D1YGlEYXSHZpk",
+    authDomain: "restaurante-ac718.firebaseapp.com",
+    projectId: "restaurante-ac718",
+    storageBucket: "restaurante-ac718.appspot.com",
+    messagingSenderId: "102787546944",
+    appId: "1:102787546944:web:a845fcdf434eed4cc284a5",
+    measurementId: "G-ZWW021BQSF"
+  };
+
+
+  export default firebaseConfig;
